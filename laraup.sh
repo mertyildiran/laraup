8ac55a6b341b13255ecbaaceb849e6ab74b441e0
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2019-08-17 05:03:45 +0300

    Initialize a new Git repository and commit the every bit of change

--- laraup.sh
+++ laraup.sh
@@ -61,7 +61,7 @@ fix_the_blade_tags () {
   done
 
   echo -e "${YELLOW}Fixing the Blade tags in ${namespace_dir}/${filename_base}${NC}";
-  php blade.php $filename
+  php $LARAUP_DIR/blade.php $filename
 }
 
 [ -z ${1+x} ] && die "${RED}Argument 1 required, $# provided${NC}"
@@ -99,31 +99,37 @@ elif [[ ! -d $TARGET_PATH ]]; then
 fi
 
 composer create-project --prefer-dist laravel/laravel $NEW_PROJECT_PATH || { echo -e "${RED}New Laravel project cloudn't be created, check the above message or your composer installation${NC}" ; exit 1; }
+cd $NEW_PROJECT_PATH && git init && git add -A . && git commit -m "Bring in Laravel 5.8 base"
 
 echo -e "\n${YELLOW}COPYING THE CONTROLLERS${NC}"
 rm -rf $NEW_PROJECT_PATH/app/Http/Controllers/*
 cp -r $OLD_PROJECT_PATH/app/controllers/* $NEW_PROJECT_PATH/app/Http/Controllers
+cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Copy the controllers"
 
 for filename in $(find $NEW_PROJECT_PATH/app/Http/Controllers -name '*.php'); do
   fix_the_namespace_of_file $filename
 done
-
+cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Fix the namespaces of the controllers"
 
 echo -e "\n${YELLOW}COPYING THE VIEWS${NC}"
 rm -rf $NEW_PROJECT_PATH/resources/views/*
 cp -r $OLD_PROJECT_PATH/app/views/* $NEW_PROJECT_PATH/resources/views
+cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Copy the views"
 
 for filename in $(find $NEW_PROJECT_PATH/resources/views -name '*.php'); do
   fix_the_blade_tags $filename
 done
+cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Fix the Blade tags"
 
 echo -e "\n${YELLOW}COPYING THE MODELS${NC}"
 mkdir -p $NEW_PROJECT_PATH/app/Models
 cp -r $OLD_PROJECT_PATH/app/models/* $NEW_PROJECT_PATH/app/Models
+cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Copy the models"
 
 for filename in $(find $NEW_PROJECT_PATH/app/Models -name '*.php'); do
   fix_the_namespace_of_file $filename
 done
+cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Fix the namespaces of the models"
 
 
 echo -e "\n${YELLOW}MIGRATING ROUTES${NC}"
@@ -132,6 +138,6 @@ ex -snc '$-1,$d|x' $NEW_PROJECT_PATH/routes/api.php
 ex -snc '$-2,$d|x' $NEW_PROJECT_PATH/routes/web.php
 php $LARAUP_DIR/routes.php $OLD_PROJECT_PATH/app/routes.php $NEW_PROJECT_PATH/routes
 echo -e "});" >> $NEW_PROJECT_PATH/routes/api.php
-
 sed -i 's/Route::controller/AdvancedRoute::controller/g' $NEW_PROJECT_PATH/routes/api.php
 sed -i 's/Route::controller/AdvancedRoute::controller/g' $NEW_PROJECT_PATH/routes/web.php
+cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Migrate the routes"
