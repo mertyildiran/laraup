5645f082e486d7c286eb5f08971380f0ce04c482
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2019-08-19 23:31:26 +0300

    Refactor the Bash scripts

--- laraup.sh
+++ laraup.sh
@@ -13,14 +13,16 @@ NC='\033[0m' # No Color
 
 source $(dirname $0)/library.sh
 
-[ -z ${1+x} ] && help_die "${RED}Argument 1 required, $# provided${NC}"
-[ -z ${2+x} ] && help_die "${RED}Argument 2 required, $# provided${NC}"
+[ -z ${1+x} ] && help_die "Argument 1 required, $# provided"
+[ -z ${2+x} ] && help_die "Argument 2 required, $# provided"
 
 OLD_PROJECT_PATH="$(realpath $1)"
 NEW_PROJECT_PATH="$(realpath $2)"
 LARAUP_DIR="$(pwd)"
 
-php $LARAUP_DIR/check.php $OLD_PROJECT_PATH 4.2 || help_die "${RED}Your Laravel version is not 4.2${NC}"
+php $LARAUP_DIR/check.php $OLD_PROJECT_PATH 4.2 || help_die "Your Laravel version is not 4.2"
+
+try_mkdir $NEW_PROJECT_PATH
 
 clear
 echo -e "${YELLOW}"
@@ -40,79 +42,71 @@ echo -e "Version: ${YELLOW}${VERSION}${NC}    Author: ${YELLOW}${AUTHOR}${NC}   
 echo -e "Path to old Laravel 4.2 project:\t${GREEN}${OLD_PROJECT_PATH}${NC}"
 echo -e "Path to new Laravel 5.8 project:\t${GREEN}${NEW_PROJECT_PATH}${NC}\n"
 
-if [[ ! -e $NEW_PROJECT_PATH ]]; then
-  mkdir -p $NEW_PROJECT_PATH
-  echo -e "${GREEN}Created directory ${NEW_PROJECT_PATH}${NC}\n"
-elif [[ ! -d $NEW_PROJECT_PATH ]]; then
-  echo -e >&2 "${RED}${NEW_PROJECT_PATH} already exists but is not a directory${NC}"
-  exit 1
-fi
-
 cd $OLD_PROJECT_PATH && composer install
 
-composer create-project --prefer-dist laravel/laravel $NEW_PROJECT_PATH || { echo -e "${RED}New Laravel project cloudn't be created, check the above message or your composer installation${NC}" ; exit 1; }
-cp -r $OLD_PROJECT_PATH/.git $NEW_PROJECT_PATH && cd $NEW_PROJECT_PATH && git checkout -b laravel-5-upgrade && git add -A . && git commit -m "Bring in Laravel 5.8 base"
+composer create-project --prefer-dist laravel/laravel $NEW_PROJECT_PATH || die "New Laravel project cloudn't be created, check the above message or your composer installation"
+cp -r $OLD_PROJECT_PATH/.git $NEW_PROJECT_PATH && cd $NEW_PROJECT_PATH && git checkout -b laravel-5-upgrade && git_commit "Bring in Laravel 5.8 base"
 
 
-echo -e "\n${YELLOW}COPYING THE CONTROLLERS${NC}"
+section_title "COPYING THE CONTROLLERS"
 rm -rf $NEW_PROJECT_PATH/app/Http/Controllers/*
 cp -r $OLD_PROJECT_PATH/app/controllers/* $NEW_PROJECT_PATH/app/Http/Controllers
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Copy the controllers"
+git_commit "Copy the controllers"
 
 for filename in $(find $NEW_PROJECT_PATH/app/Http/Controllers -name '*.php'); do
   fix_the_namespace_of_file $filename
 done
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Fix the namespaces of the controllers"
+git_commit "Fix the namespaces of the controllers"
 
 for filename in $(find $NEW_PROJECT_PATH/app/Http/Controllers/ -type f -exec grep -l "extends Controller" {} \;); do
   fix_the_base_controller $filename
 done
 
 find $NEW_PROJECT_PATH/app/Http/Controllers/ -type f -exec sed -i 's/extends \\BaseController/extends BaseController/g' {} +
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Fix the controllers that incorrectly trying to extend from base controller"
+git_commit "Fix the controllers that incorrectly trying to extend from base controller"
 
 
-echo -e "\n${YELLOW}COPYING THE VIEWS${NC}"
+section_title "COPYING THE VIEWS"
 rm -rf $NEW_PROJECT_PATH/resources/views/*
 cp -r $OLD_PROJECT_PATH/app/views/* $NEW_PROJECT_PATH/resources/views
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Copy the views"
+git_commit "Copy the views"
 
 for filename in $(find $NEW_PROJECT_PATH/resources/views -name '*.php'); do
   fix_the_blade_tags $filename
 done
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Fix the Blade tags"
+git_commit "Fix the Blade tags"
 
 cd $NEW_PROJECT_PATH && composer require laravelcollective/html && \
   git add -A . && git commit -m "Composer require laravelcollective/html"
 
 
-echo -e "\n${YELLOW}COPYING THE MODELS${NC}"
+section_title "COPYING THE MODELS"
 mkdir -p $NEW_PROJECT_PATH/app/Models
 cp -r $OLD_PROJECT_PATH/app/models/* $NEW_PROJECT_PATH/app/Models
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Copy the models"
+git_commit "Copy the models"
 
 for filename in $(find $NEW_PROJECT_PATH/app/Models -name '*.php'); do
   fix_the_namespace_of_file $filename
 done
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Fix the namespaces of the models"
+git_commit "Fix the namespaces of the models"
 
 find $NEW_PROJECT_PATH/app/Models/ -type f -exec sed -i 's/SoftDeletingTrait/SoftDeletes/g' {} +
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Rename SoftDeletingTrait usages as SoftDeletes in app/Models/"
+git_commit "Rename SoftDeletingTrait usages as SoftDeletes in app/Models/"
 
 
-echo -e "\n${YELLOW}COPYING THE MIGRATIONS & SEEDS${NC}"
+section_title "COPYING THE MIGRATIONS & SEEDS"
 rm $NEW_PROJECT_PATH/database/migrations/*
 cp -r $OLD_PROJECT_PATH/app/database/migrations/* $NEW_PROJECT_PATH/database/migrations
 rsync -a $OLD_PROJECT_PATH/app/database/seeds/* $NEW_PROJECT_PATH/database/seeds/ --exclude=DatabaseSeeder.php
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Copy the migrations and seeds"
+git_commit "Copy the migrations and seeds"
 
 
-echo -e "\n${YELLOW}COPYING THE PUBLIC FILES${NC}"
+section_title "COPYING THE PUBLIC FILES"
 rsync -a $OLD_PROJECT_PATH/public/* $NEW_PROJECT_PATH/public/ --exclude=index.php --exclude=.htaccess
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Copy the public files"
+git_commit "Copy the public files"
 
 
-echo -e "\n${YELLOW}MIGRATING ROUTES${NC}"
+section_title "MIGRATING ROUTES"
 cd $NEW_PROJECT_PATH && composer require lesichkovm/laravel-advanced-route
 ex -snc '$-1,$d|x' $NEW_PROJECT_PATH/routes/api.php
 ex -snc '$-2,$d|x' $NEW_PROJECT_PATH/routes/web.php
@@ -120,89 +114,89 @@ php $LARAUP_DIR/routes.php $OLD_PROJECT_PATH/app/routes.php $NEW_PROJECT_PATH/ro
 echo -e "});" >> $NEW_PROJECT_PATH/routes/api.php
 sed -i 's/Route::controller/AdvancedRoute::controller/g' $NEW_PROJECT_PATH/routes/api.php
 sed -i 's/Route::controller/AdvancedRoute::controller/g' $NEW_PROJECT_PATH/routes/web.php
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Migrate the routes"
+git_commit "Migrate the routes"
 
 
-echo -e "\n${YELLOW}COPYING app/helpers.php${NC}"
+section_title "COPYING app/helpers.php"
 cp -r $OLD_PROJECT_PATH/app/helpers.php $NEW_PROJECT_PATH/app/
 ex -e -s -c ":%s/function info/function info_deprecated/g" $NEW_PROJECT_PATH/app/helpers.php -c wq
 php $LARAUP_DIR/helpers.php $NEW_PROJECT_PATH
 cd $NEW_PROJECT_PATH && composer dump-autoload
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Copy app/helpers.php"
+git_commit "Copy app/helpers.php"
 
 
-echo -e "\n${YELLOW}TURNING FILTERS INTO MIDDLEWARES${NC}"
+section_title "TURNING FILTERS INTO MIDDLEWARES"
 php $LARAUP_DIR/filters.php $OLD_PROJECT_PATH/app/filters.php $NEW_PROJECT_PATH
 find $NEW_PROJECT_PATH/app/Http/Controllers/ -type f -exec sed -i 's/beforeFilter(/middleware(/g' {} +
 find $NEW_PROJECT_PATH/app/Http/Controllers/ -type f -exec sed -i 's/afterFilter(/middleware(/g' {} +
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Turn filters into middlewares"
+git_commit "Turn filters into middlewares"
 
 
-echo -e "\n${YELLOW}FIXING THE ADDITIONAL ISSUES IN THE CONTROLLERS${NC}"
+section_title "FIXING THE ADDITIONAL ISSUES IN THE CONTROLLERS"
 find $NEW_PROJECT_PATH/app/Http/Controllers/ -type f -exec sed -i 's/App::make\(.*\);/app();/g' {} +
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Fix the additional issues in the controllers"
+git_commit "Fix the additional issues in the controllers"
 
 
-cd $NEW_PROJECT_PATH && composer dump-autoload || die "\n${RED}New Composer packages couldn't be installed because \"composer dump-autoload\" is failed. Fix this issue and continue to run ${LARAUP_DIR}/laraup.sh from line: ${LINENO}${NC}"
-echo -e "\n${YELLOW}INSTALLING NEW COMPOSER PACKAGES${NC}"
-echo -e "${YELLOW}CAUTION: If you have added some secret repositories then you might have to enter your passphrase a few times on this step. So don't just assume that's an infinite loop.${NC}"
+cd $NEW_PROJECT_PATH && composer dump-autoload || die "\nNew Composer packages couldn't be installed because \"composer dump-autoload\" is failed. Fix this issue and continue to run ${LARAUP_DIR}/laraup.sh from line: ${LINENO}"
+section_title "INSTALLING NEW COMPOSER PACKAGES"
+echo_warning "CAUTION: Run ssh-add otherwise it will keep asking to enter your passphrase a few times in case of secret repositories."
 php $LARAUP_DIR/composer.php $OLD_PROJECT_PATH $NEW_PROJECT_PATH
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Install new Composer packages"
+git_commit "Install new Composer packages"
 
 
-echo -e "\n${YELLOW}COPYING ARTISAN COMMANDS${NC}"
+section_title "COPYING ARTISAN COMMANDS"
 mkdir -p $NEW_PROJECT_PATH/app/Console/Commands
 cp -r $OLD_PROJECT_PATH/app/commands/* $NEW_PROJECT_PATH/app/Console/Commands
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Copy Artisan commands"
+git_commit "Copy Artisan commands"
 
 for filename in $(find $NEW_PROJECT_PATH/app/Console/Commands -name '*.php'); do
   fix_the_namespace_of_file $filename
 done
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Fix the namespaces of Artisan commands"
+git_commit "Fix the namespaces of Artisan commands"
 
 find $NEW_PROJECT_PATH/app/Console/Commands/ -type f -exec sed -i 's/public function fire()/public function handle()/g' {} +
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Rename the methods named fire() to handle() in Artisan commands"
+git_commit "Rename the methods named fire() to handle() in Artisan commands"
 
 
-echo -e "\n${YELLOW}FIXING THE MIGRATIONS & SEEDS${NC}"
+section_title "FIXING THE MIGRATIONS & SEEDS"
 
 for filename in $(find $NEW_PROJECT_PATH/database/migrations -name '*.php'); do
   fix_the_migration $filename
 done
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Fix the migrations"
+git_commit "Fix the migrations"
 
 for filename in $(find $NEW_PROJECT_PATH/database/seeds -name '*.php' ! -name 'DatabaseSeeder.php'); do
   fix_the_seed $filename
 done
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Fix the seeds"
+git_commit "Fix the seeds"
 
 
-echo -e "\n${YELLOW}COPYING THE LANGUAGE FILES${NC}"
+section_title "COPYING THE LANGUAGE FILES"
 rsync -a $OLD_PROJECT_PATH/app/lang/* $NEW_PROJECT_PATH/resources/lang/
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Copy the language files"
+git_commit "Copy the language files"
 
 
-echo -e "\n${YELLOW}COPYING THE TESTS${NC}"
+section_title "COPYING THE TESTS"
 rsync -a $OLD_PROJECT_PATH/app/tests/* $NEW_PROJECT_PATH/tests/
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Copy the tests"
+git_commit "Copy the tests"
 
 
-echo -e "\n${YELLOW}COPYING .env files${NC}"
+section_title "COPYING .env files"
 yes | cp -rf $OLD_PROJECT_PATH/.env* $NEW_PROJECT_PATH/
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Copy .env files"
+git_commit "Copy .env files"
 
 
-echo -e "\n${YELLOW}COPYING THE CONFIGURATION FILES${NC}"
+section_title "COPYING THE CONFIGURATION FILES"
 rsync -r --ignore-existing $OLD_PROJECT_PATH/app/config/* $NEW_PROJECT_PATH/config/ --exclude=/app.php
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Copy the configuration files"
+git_commit "Copy the configuration files"
 
 php $LARAUP_DIR/config.php $OLD_PROJECT_PATH $NEW_PROJECT_PATH
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Fix config/app.php file"
+git_commit "Fix config/app.php file"
 
 
-echo -e "\n${YELLOW}COPYING THE REMAINING FILES${NC}"
+section_title "COPYING THE REMAINING FILES"
 rsync -a --exclude={'.git','app','bootstrap','public','vendor','.env*','artisan','composer.*','package.json','package-lock.json','server.php'} $OLD_PROJECT_PATH/* $NEW_PROJECT_PATH/
 rsync -r --ignore-existing $OLD_PROJECT_PATH/*.md $NEW_PROJECT_PATH/
-cd $NEW_PROJECT_PATH && git add -A . && git commit -m "Copy the remaining files"
+git_commit "Copy the remaining files"
 
-echo -e "\n\n${GREEN}Congragulations! \\( ﾟヮﾟ)/ Your Laravel 4.2 project has been upgraded to 5.8 successfully. You can find your upgraded project in here:${NC} ${NEW_PROJECT_PATH}\n"
+echo_success "\n\nCongragulations! \\( ﾟヮﾟ)/ Your Laravel 4.2 project has been upgraded to 5.8 successfully. You can find your upgraded project in here:${NC} ${NEW_PROJECT_PATH}\n"
--- library.sh
+++ library.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 help_die () {
-  echo -e >&2 "$@"
+  echo_error $@
   printf "\n"
   help
   exit 1
@@ -19,13 +19,42 @@ EOF
 }
 
 die () {
-  echo -e >&2 "$@"
+  echo_error $@
   printf "\n"
   exit 1
 }
 
+section_title () {
+  echo -e "\n${YELLOW}$@${NC}"
+}
 
-fix_the_namespace_of_file () {
+echo_warning () {
+  echo -e "${YELLOW}$@${NC}"
+}
+
+echo_success () {
+  echo -e "${GREEN}$@${NC}"
+}
+
+echo_error () {
+  echo -e >&2 "${RED}$@${NC}"
+}
+
+git_commit () {
+  cd $NEW_PROJECT_PATH && git add -A . && git commit -m "$@"
+}
+
+try_mkdir() {
+  if [[ ! -e $@ ]]; then
+    mkdir -p $@
+    echo -e "${GREEN}Created directory $@${NC}\n"
+  else
+    die "$@ already exists"
+    exit 1
+  fi
+}
+
+parse_namespace () {
   filename=$1
   [ -f "$filename" ] || continue
   filename_dir=$(dirname "${filename}")
@@ -38,6 +67,10 @@ fix_the_namespace_of_file () {
     [ -z "$namespace" ] || namespace="${namespace}\\\\"
     namespace="${namespace}${i^}"
   done
+}
+
+fix_the_namespace_of_file () {
+  parse_namespace $1
 
   echo -e "${YELLOW}Fixing the namespace of ${namespace_dir}/${filename_base}${NC}";
   sed -i '/^namespace/d' $filename
@@ -46,36 +79,14 @@ fix_the_namespace_of_file () {
 }
 
 fix_the_blade_tags () {
-  filename=$1
-  [ -f "$filename" ] || continue
-  filename_dir=$(dirname "${filename}")
-  filename_base=$(basename "${filename}")
-  namespace_dir=${filename_dir#"$NEW_PROJECT_PATH/"}
-
-  namespace=""
-  IFS='/' read -ra ADDR <<< "$namespace_dir"
-  for i in "${ADDR[@]}"; do
-    [ -z "$namespace" ] || namespace="${namespace}\\\\"
-    namespace="${namespace}${i^}"
-  done
+  parse_namespace $1
 
   echo -e "${YELLOW}Fixing the Blade tags in ${namespace_dir}/${filename_base}${NC}";
   php $LARAUP_DIR/blade.php $filename
 }
 
 fix_the_base_controller () {
-  filename=$1
-  [ -f "$filename" ] || continue
-  filename_dir=$(dirname "${filename}")
-  filename_base=$(basename "${filename}")
-  namespace_dir=${filename_dir#"$NEW_PROJECT_PATH/"}
-
-  namespace=""
-  IFS='/' read -ra ADDR <<< "$namespace_dir"
-  for i in "${ADDR[@]}"; do
-    [ -z "$namespace" ] || namespace="${namespace}\\\\"
-    namespace="${namespace}${i^}"
-  done
+  parse_namespace $1
 
   echo -e "${YELLOW}Fixing the base controller ${namespace_dir}/${filename_base}${NC}";
   sed -i "3 ause Illuminate\\\\Routing\\\\Controller;" $filename
@@ -85,18 +96,7 @@ fix_the_base_controller () {
 }
 
 fix_the_migration () {
-  filename=$1
-  [ -f "$filename" ] || continue
-  filename_dir=$(dirname "${filename}")
-  filename_base=$(basename "${filename}")
-  namespace_dir=${filename_dir#"$NEW_PROJECT_PATH/"}
-
-  namespace=""
-  IFS='/' read -ra ADDR <<< "$namespace_dir"
-  for i in "${ADDR[@]}"; do
-    [ -z "$namespace" ] || namespace="${namespace}\\\\"
-    namespace="${namespace}${i^}"
-  done
+  parse_namespace $1
 
   echo -e "${YELLOW}Fixing the migration ${namespace_dir}/${filename_base}${NC}";
   #sed -i "2 ause Illuminate\\\\Database\\\\Schema\\\\Blueprint;" $filename
@@ -105,18 +105,7 @@ fix_the_migration () {
 }
 
 fix_the_seed () {
-  filename=$1
-  [ -f "$filename" ] || continue
-  filename_dir=$(dirname "${filename}")
-  filename_base=$(basename "${filename}")
-  namespace_dir=${filename_dir#"$NEW_PROJECT_PATH/"}
-
-  namespace=""
-  IFS='/' read -ra ADDR <<< "$namespace_dir"
-  for i in "${ADDR[@]}"; do
-    [ -z "$namespace" ] || namespace="${namespace}\\\\"
-    namespace="${namespace}${i^}"
-  done
+  parse_namespace $1
 
   echo -e "${YELLOW}Fixing the seed ${namespace_dir}/${filename_base}${NC}";
   sed -i "2 ause Illuminate\\\\Database\\\\Seeder;" $filename
